4cb83779e96d13e6c766a203726cb4f95293cbdd
Luminesence <101959214+LuminesenceProject@users.noreply.github.com>
2022-04-08 23:18:01 -0400

    Delete Luminesence Website directory

--- Luminesence Website/Games/sandgame/canvasConfig.js
+++
@@ -1,1 +0,0 @@
-const __max_width=700,__max_height=700,width=Math.min(560,Math.max(screen.width-6,1)),height=Math.min(480,Math.max(screen.height-200,100)),MAX_FPS=120,DEFAULT_FPS=60,MAX_NUM_PARTICLES=1e3;
