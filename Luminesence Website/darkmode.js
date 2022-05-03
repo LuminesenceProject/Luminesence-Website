a9972e737f4fc7bf0a32e88a2b233067288dfa12
Luminesence <101959214+LuminesenceProject@users.noreply.github.com>
2022-05-02 21:25:48 -0400

    Darkmode.js

---
+++ Luminesence Website/darkmode.js
@@ -0,0 +1,23 @@
+//THIS CODE MAY BE USED ON ANY WEBSITE
+//AS LONG AS CREDIT IS CLEARLY GIVEN
+
+window.alert("This will break some websites");
+
+function toggle() {
+    let q = document.querySelectorAll('#nightify')
+    if(q.length) {
+        q[0].parentNode.removeChild(q[0])
+        return false
+    }
+    var h = document.getElementsByTagName('head')[0],
+        s = document.createElement('style');
+    s.setAttribute('type', 'text/css');
+    s.setAttribute('id', 'nightify');
+    s.appendChild(document.createTextNode('html{-webkit-filter:invert(100%) hue-rotate(180deg) contrast(70%) !important; background: #fff;} .line-content {background-color: #fefefe;}'));
+    h.appendChild(s); 
+    return true
+}
+
+var result = toggle();
+
+localStorage.toggle = true;
