c8b124611333e2da5b9e99dcb353758960d5ff9c
Luminesence <101959214+LuminesenceProject@users.noreply.github.com>
2022-09-05 21:04:30 -0400

    Update canvasConfig.js

--- Luminesence Website/Games/sandgame/canvasConfig.js
+++ Luminesence Website/Games/sandgame/canvasConfig.js
@@ -1,6 +1,6 @@
 
-const __max_width = 1000;
-const __max_height = 500;
+const __max_width = 550;
+const __max_height = 600;
 
 /* Need to use a smaller size when on mobile devices with small screens */
 const width = Math.min(__max_width, Math.max(screen.width - 6, 1));
